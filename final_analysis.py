e0727bfbf7cc3c0a3b182831239a90dcbcbd553f
znordan2 <znordan2@illinois.edu>
2025-12-13 19:52:44 -0600

    more of the same

--- final_analysis.py
+++ final_analysis.py
@@ -8,6 +8,7 @@ import os
 #data directories
 shadowbar_data_directory = "PuBe_ToF_12_8_25_Shadowbar"
 bare_data_directory = "PuBe_ToF_12_4_25_Bare/Filtered"
+tof_calibration_data_directory = "TOF_calibration_Na22"
 
 #creating necessary directories
 final_plottting_directory = "Final_Analysis_Plots"
@@ -23,14 +24,15 @@ mass_neutron = 1.675e-27 # kg
 bare_measurement_time = 47 * 3600 + 7 * 60 + 29
 shadowbar_measurement_time = 48 * 3600 + 1
 
+minimum_energy_time = 67.06 # ns
+maximum_energy_time = 23.4257 # ns
 
 
-#expected highest peak energy and time
-expected_highest_peak_time = 23.4257 # ns
+
 
 #retrieving TOF spectra from both datasets
 tof_spectra = []
-for data_directory in [shadowbar_data_directory, bare_data_directory]:
+for data_directory in [shadowbar_data_directory, bare_data_directory, tof_calibration_data_directory]:
     filtered_files = sorted(os.listdir(data_directory))
     for file in filtered_files:
         spectrum_type = file.split("_")[2]
@@ -42,48 +44,116 @@ for data_directory in [shadowbar_data_directory, bare_data_directory]:
 
 shadowbar_tof_data = tof_spectra[0]
 bare_tof_data = tof_spectra[1]
+calibration_tof_data = tof_spectra[2]
 
 #normalizing data by measurement time
-shadowbar_tof_data = shadowbar_tof_data / shadowbar_measurement_time
-bare_tof_data = bare_tof_data / bare_measurement_time
+shadowbar_tof_data = shadowbar_tof_data #/ shadowbar_measurement_time
+bare_tof_data = bare_tof_data #/ bare_measurement_time
 
 
 #applying boxcar moving average filter to data to smooth out fluctuations
 shadowbar_tof_data = boxcar_average_numpy(shadowbar_tof_data, window_size=5)
 bare_tof_data = boxcar_average_numpy(bare_tof_data, window_size=5)
 
+
+
+plt.plot(bare_tof_data, label = "Bare Spectrum")
+plt.plot(shadowbar_tof_data, label = "Shadowbar Spectrum")
+plt.plot(calibration_tof_data, label = "Calibration Spectrum")
+plt.yscale('log')
+plt.title("Raw TOF Spectra")
+# plt.show()
+plt.clf()
 net_data = bare_tof_data - shadowbar_tof_data
-channels = np.arange(len(net_data))
-time = channels * time_conversion_factor
 
-#finding where net data is not zero
-where_not_0 = np.where(net_data != 0)
-net_data = net_data[where_not_0]
-time = time[where_not_0]
+peak_channel_calibration = np.argmax(calibration_tof_data)
+peak_channel_bare = np.argmax(bare_tof_data)
+peak_channel_shadowbar = np.argmax(shadowbar_tof_data)
 
-#large peak index & finding baseline average
-large_peak_index = np.argmax(net_data)
-baseline_average = np.mean(net_data[0:large_peak_index-20])
+print(f"Na22 Peak Channel: {peak_channel_calibration}")
+print(f"Bare Peak Channel: {peak_channel_bare}")
+print(f"Shadowbar Peak Channel: {peak_channel_shadowbar}")
 
-large_peak_channel = where_not_0[0][large_peak_index]
-# time = time * expected_highest_peak_time / large_peak_channel
+channels = np.arange(len(calibration_tof_data))
+channels -= peak_channel_calibration
 
-#remvoing baseline average from net data
-net_data = net_data - baseline_average
-net_data = np.where(net_data < 0, 0, net_data) * 48 * 3600  # converting to counts per 48 hours
+# time_ns_calibration_factor = maximum_energy_time / (channels[peak_channel_bare] - channels[peak_channel_calibration])
+time_ns_calibration_factor = maximum_energy_time / (channels[3850] - channels[peak_channel_calibration])
 
-net_data2 = boxcar_average_numpy(net_data, window_size=1000)
+print(channels[peak_channel_bare] - channels[peak_channel_calibration])
+print(f"Calibration Factor (ns/channel): {time_ns_calibration_factor}")
 
+time_ns = channels * time_ns_calibration_factor
 
-
-
-plt.plot(time, net_data,  label="Net Spectrum (Bare - Shadowbar)")
-# plt.plot(time, net_data2, label="Smoothed Net Spectrum")
+plt.plot(time_ns, bare_tof_data, label = "Bare Spectrum")
+plt.plot(time_ns, shadowbar_tof_data, label = "Shadowbar Spectrum")
+plt.axvline(x = time_ns[peak_channel_shadowbar], color='r', linestyle='--', label=f'Shadowbar Peak: {time_ns[peak_channel_shadowbar]:.2f} ns')
+# plt.axvline(x = time_ns[3850])
 plt.legend()
-plt.xlabel("Time of Flight (ns)")
-plt.ylabel("Counts per Second")
-# plt.axhline(y=baseline_average, color='r', linestyle='--', label="Baseline Average")
-# plt.yscale("log")
+plt.yscale('log')
+plt.ylim(bottom = 1e1)
+plt.xlim(-1500, 2000)
 plt.show()
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+where_not_0 = np.where(net_data>0)
+net_data = net_data[where_not_0]
+channels = np.arange(len(net_data))
+big_peak_index = np.argmax(net_data)
+end_of_left_contium_index = big_peak_index -50
+start_of_left_contium_index = end_of_left_contium_index - 360
+index_of_0 = round(np.average([start_of_left_contium_index, end_of_left_contium_index]))
+
+baseline_average = np.average(net_data[start_of_left_contium_index:end_of_left_contium_index])
+net_data = net_data - baseline_average
+net_data = np.where(net_data <0, 0, net_data)
+
+channels = channels - index_of_0
+
+channel_to_ns = maximum_energy_time / channels[big_peak_index]
+time_ns = channels * channel_to_ns
+
+plt.plot(time_ns,net_data)
+# plt.axvline(x=end_of_left_contium_index,color='r')
+# plt.axvline(x=start_of_left_contium_index,color='r')
+plt.axvline(x=time_ns[index_of_0],color='g', label = "zero point", linestyle='--')
+plt.axvline(x=time_ns[big_peak_index],color='b', label = "6.4 MeV peak", linestyle='--')
+plt.xlabel("Time-of-Flight (ns)")
+plt.ylabel("Counts (Normalized)")
+# plt.yscale('log')
+# plt.show()
+plt.clf()
+where_time_positive = np.where(time_ns > 0)
+time_ns = time_ns[where_time_positive]
+net_data = net_data[where_time_positive]
+
+
+energy_data = time_of_flight(distance, time_ns, mass_neutron, "m", "ns")[1] * joule_to_ev / 1e6
+plt.plot(energy_data, net_data)
+plt.xlim(0,12)
+plt.xlabel("Neutron Energy (MeV)")
+plt.ylabel("Counts (Normalized)")
+# plt.show()
+
+
+'''
